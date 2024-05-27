faeb4d731a4b57c19d7e3f01e13fffac9d61984a
Gabriel Ulises Martinez Gutierrez <ulisesuliiuwu@gmil.com>
2024-05-27 17:15:35 -0600

    Auth para aplicacion movil

---
+++ src/test/java/com/LoveArt/ServiceAuth/ServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoveArt.ServiceAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
